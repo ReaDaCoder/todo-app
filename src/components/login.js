0283f159834bd547149c5b0ddb5822b830db4684
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-07-22 15:46:16 +0200

    Added content for login page

---
+++ src/components/login.js
@@ -0,0 +1,24 @@
+function Login(){
+    return(
+        <div>
+        <h1>Log in to your Account</h1>
+        <div className="login-box">
+            <div className="box1">
+            <form>
+            <input type="text" placeholder="Email"/>
+            <input type="text" placeholder="Password"/>
+            <button>Log in</button>
+        </form>
+        <button>Go to Home page</button>
+            </div>
+            <div className="box2">
+                <h1>Tak Managaement</h1>
+                <img src="/media/todo-pic.jpg"/>
+            </div>
+        </div>
+        
+    </div>
+    )
+}
+    
+    
