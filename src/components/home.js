246246ca7fe63d37400a084be671e9e9b06283b0
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-07-23 15:27:07 +0200

    added img for tick and css

--- src/components/home.js
+++ src/components/home.js
@@ -16,21 +16,32 @@ function Home(){
 
 
     return(
-        <div class="container">
-            <h2>To-Do List  <img src="/media/icon.PNG" width="30px"/></h2>
+        <div class="container-for-tasks">
+            <h2>To-Do List  <img src="/media/icon-todo.PNG" width="70px"/></h2>
                 <h3>Date</h3>
                 <h3>Time</h3>
                 <div class="input-grid">
                 <input type="text" placeholder="Task Name" class="filter-input"/>
                 <button class="filter-btn">Add</button>
                 </div>
-            <div className="todo-grid">
-                <div className="task-name">Task Name</div>
-                <div className="definition">Definition</div>
-                <div className="priority">Priority</div>
-                <div className="due">Due date</div>
-            </div>
+            <ul id="list-container">
+                <li class="ticked">Task 1</li>
+                <li>Task 2</li>
+                <li>Task 3</li>
+                <li>Task 4</li>
+                <li>Task 5</li>
+            </ul>
         </div>
     )
 }
 export default Home
+
+
+
+
+/*<div className="todo-grid">
+                <div className="task-name">Task Name</div>
+                <div className="definition">Definition</div>
+                <div className="priority">Priority</div>
+                <div className="due">Due date</div>
+            </div>*/
