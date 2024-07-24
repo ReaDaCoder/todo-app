2268627470e3f566bd2b768639f5b1b908ba14f5
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-07-24 21:37:48 +0200

    added event listener

--- src/components/home.js
+++ src/components/home.js
@@ -1,40 +1,55 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-function Home(){
-
-    const [tasks, setTask] = useState(() => {
+function Home() {
+    const [input, setInput] = useState('');
+    const [tasks, setTasks] = useState(() => {
         const storedTasks = localStorage.getItem('tasks');
         return storedTasks ? JSON.parse(storedTasks) : [];
     });
+
     const navigate = useNavigate();
 
     useEffect(() => {
         localStorage.setItem('tasks', JSON.stringify(tasks));
     }, [tasks]);
 
+    const handleAddTask = () => {
+        if (input.trim() === '') {
+            alert('Enter task!');
+        } else {
+            setTasks([...tasks, input.trim()]);
+            setInput('');
+        }
+    };
 
-
-    return(
-        <div class="container-for-tasks">
-            <h2>To-Do List  <img src="/media/icon-todo.PNG" width="70px"/></h2>
-                <h3>Date</h3>
-                <h3>Time</h3>
-                <div class="input-grid">
-                <input type="text" placeholder="Task Name" class="filter-input"/>
-                <button class="filter-btn">Add</button>
-                </div>
+    return (
+        <div className="container-for-tasks">
+            <h2>To-Do List <img src="/media/icon-todo.PNG" width="70px" alt="Todo Icon" /></h2>
+            <h3>Date</h3>
+            <h3>Time</h3>
+            <div className="input-grid">
+                <input 
+                    type="text" 
+                    placeholder="Task Name" 
+                    id="input-box" 
+                    className="filter-input"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                />
+                <button className="filter-btn" onClick={handleAddTask}>Add</button>
+            </div>
             <ul id="list-container">
-                <li class="ticked">Task 1</li>
-                <li>Task 2</li>
-                <li>Task 3</li>
-                <li>Task 4</li>
-                <li>Task 5</li>
+                {tasks.map((task, index) => (
+                    <li key={index}>{task}</li>
+                ))}
             </ul>
         </div>
-    )
+    );
 }
-export default Home
+
+export default Home;
+
 
 
 
